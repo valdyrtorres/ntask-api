85a302c5c3c9e965e07772c656993e1e645f3c38
Valdir Borges <Valdir.Borges@match.com>
2018-07-13 18:58:22 -0300

    10. capitulo

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express"
+import consign from "consign"
+
+const app = express();
+
+app.set("json spaces", 4);
+
+consign()
+   .include("models")
+   .then("libs/middleware.js")
+   .then("routes")
+   .then("libs/boot.js")
+   .into(app);
+
